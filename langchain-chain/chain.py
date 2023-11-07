6506c5386951c04cc0847abea2f2540eaa1bd5d8
Harry Wang <harryjwang@gmail.com>
2023-11-07 10:29:31 -0500

    added tutorials

--- langchain-chain/chain.py
+++ langchain-chain/chain.py
@@ -6,6 +6,7 @@ from prompts import description_prompt, script_prompt
 import langchain
 langchain.verbose = True
 # loads the .env file
+from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo")
